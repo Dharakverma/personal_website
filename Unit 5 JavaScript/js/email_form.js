e8fbba224bc488bb92a1dcb499598131cb2ce1fe
Dharak Verma <vermad1@mcmaster.ca>
2023-01-12 23:44:12 -0500

    Refactor search bar code

---
+++ Unit 5 JavaScript/js/email_form.js
@@ -0,0 +1,58 @@
+// collect form element and user input elements
+emailForm = document.getElementById('email-form');
+formInputs = emailForm.elements;
+nameInput = formInputs.name;
+emailInput = formInputs.email;
+messageInput = formInputs.message;
+
+// Define a function to validate the input values
+const validateInputs = (inputs) => {
+    // Create an array to store the input values
+    const inputValues = [];
+
+    // Iterate over the input elements
+    for (const input of inputs) {
+        // If the value of the input is an empty string after being trimmed, return false
+        if (input.value.trim() === '') {
+            return false;
+        }
+        // Otherwise, add the value to the array of input values
+        inputValues.push(input.value.trim());
+    }
+    // Return the array of input values if all of the inputs are present
+    return inputValues;
+};
+
+// Define a function to send the email
+const sendEmail = (event) => {
+    // Prevent the form from submitting and reloading the page
+    event.preventDefault();
+
+    // Validate the input values
+    const inputValues = validate
+    Inputs(formInputs);
+
+    // If the input values are not valid, show an error message and return
+    if (!inputValues) {
+        alert('Please fill out all fields');
+        return;
+    }
+
+    // Destructure the input values into separate variables
+    const [name, email, message] = inputValues;
+
+    // Use the EmailJS library to send an email with the input values as the sender's name, email address, and message
+    Email.send({
+        SecureToken: 'your-secure-token',
+        To: 'your@email.com',
+        From: email,
+        Subject: 'Message from ' + name,
+        Body: message
+    }).then(() => {
+        // Show a success message if the email was sent successfully
+        alert('Your message was sent successfully');
+    });
+};
+
+// Add an event listener to the form element that listens for the submit event and calls the sendEmail function
+emailForm.addEventListener('submit', sendEmail);
--- Unit 5 JavaScript/js/site_search.js
+++ Unit 5 JavaScript/js/site_search.js
@@ -1,90 +1,84 @@
 // Object to store all search results, hyperlinks to redirect user, and relevant keywords to display search result(s)
-const pages = [
+const data = [
     {
         title: "Embedded Architecture",
         url: "embedded_projects/vehicle_architecture.html",
-        content: "2020 - present competition vehicle embedded systems architecture, design decisions behind it, and details on each component.",
+        description: "2020 - present competition vehicle embedded systems architecture, design decisions behind it, and details on each component.",
         keywords: ["embedded", "architecture", "ECU", "electronic control unit", "STM", "ARM", "microcontroller", "MCU", "design", "decisions"]
     },
     {
         title: "Controls Architecture",
         url: "controls_projects/controls_architecture.html",
-        content: "2022 - 2023 simulink-based vehicle control system architecture, design decisions behind it, and details on each component.",
+        description: "2022 - 2023 simulink-based vehicle control system architecture, design decisions behind it, and details on each component.",
         keywords: ["controls", "architecture", "simulink", "matlab", "block design", "testing", "test", "white box", "black box", "decisions", "design"]
     },
     {
         title: "Communications Manager",
         url: "embedded_projects/work_in_progress.html",
-        content: "Communications layer on top of pre-existing hardware abstraction layer (HAL), used to modify and fix any shortcomings in the HAL and allow for extensive unit testing.",
+        description: "Communications layer on top of pre-existing hardware abstraction layer (HAL), used to modify and fix any shortcomings in the HAL and allow for extensive unit testing.",
         keywords: ["C", "Python", "testing", "unit", "communications", "CAN", "I2C", "UART", "SPI", "STM"]
     },
     {
         title: "Torque Path Controller",
         url: "embedded_projects/work_in_progress.html",
-        content: "Torque controller component inside the Simulink based control system.",
+        description: "Torque controller component inside the Simulink based control system.",
         keywords: ["torque", "controls", "architecture", "simulink", "matlab", "block design", "testing", "test", "white box", "black box", "decisions", "design", "controller", "motors"]
     },
     {
         title: "Battery Monitor",
         url: "embedded_projects/work_in_progress.html",
-        content: "Battery monitor component inside the Simulink based control system.",
+        description: "Battery monitor component inside the Simulink based control system.",
         keywords: ["controls", "architecture", "simulink", "matlab", "block design", "testing", "test", "white box", "black box", "decisions", "design", "BMS", "battery pack", "management"]
     },
     {
         title: "Scheduler-From-Scratch",
         url: "embedded_projects/work_in_progress.html",
-        content: "In-house designed and developed operating system scheduler, responsible for task management and pre-emption for all vehicle tasks.",
+        description: "In-house designed and developed operating system scheduler, responsible for task management and pre-emption for all vehicle tasks.",
         keywords: ["C", "STM", "designed", "developed", "operating system", "OS", "RTOS", "scheduler", "tasks"]
     }
 ];
 
-/**
- * searchPages will search through the pages array and find matching results based on any relevant keyword or part of a keyword
- * @param {String} searchQuery - The search query
- * @returns {Array} - All matching website pages
- */
- function searchPages(searchQuery) {
-    // Create a regular expression from the search query
-    const regex = new RegExp(searchQuery, 'i');
-    // Use the Array.prototype.filter() method to find pages that match the search query
-    return pages.filter(page => regex.test(page.title) || regex.test(page.content) || page.keywords.some(keyword => regex.test(keyword)));
+// searchForResults will search through the pages array and find matching results based on any relevant keyword or part of a keyword
+function searchForResults(searchQuery) {
+    // Create a regular expression from the input searchQuery
+    // Make all input text lowercase for accuracy
+    regexValue = new RegExp(searchQuery, 'i');
+    // Find and return all webpages that match the search result
+    // Search through the title, description, and keywords
+    return data.filter(page => regexValue.test(page.title)
+        || regexValue.test(page.description)
+        || page.keywords.some(keyword => regexValue.test(keyword)));
 }
 
-/**
- * results will display the search results
- * @param {Array} results - All matching website pages
- */
+// displayResults will display the search results
 function displayResults(results) {
-    // Capture the results container
+    // Handle results container
     const resultsContainer = document.getElementById("resultsContainer");
-    // Clear previous search results
     resultsContainer.innerHTML = "";
-    // Iterate over results array and add each result to search results container
+
+    // Add each result to the results container
     results.forEach(result => {
-        // Create new link element
-        const link = document.createElement('a');
+        // Create new display elements
+        link = document.createElement('a');
+        description = document.createElement('p');
+
+        // Add relevant webpage redirects to the search result link
         link.href = result.url;
         link.innerText = result.title;
-        // Append link to search results container
+
+        // Append results to search results container on webpage
         resultsContainer.appendChild(link);
-        // Create element to display the content of the result
-        const content = document.createElement('p');
-        content.innerText = result.content;
-        // Append searc]h result information/content to search results container
-        resultsContainer.appendChild(content);
+        description.innerText = result.description;
+        resultsContainer.appendChild(description);
     });
 }
 
-/**
- * handleSearch will handle the search form submission on user input
- */
-function handleSearch() {
+// searchFormHandler will handle the search form submission on user input
+function searchFormHandler() {
     // Capture the input field
     const input = document.getElementById("searchInput");
     // Store the search query
     const searchQuery = input.value;
-    // Use searchPages() to find matching results
-    const results = searchPages(searchQuery);
-    // Use displayResults() to display the matching results
-    displayResults(results);
+
+    displayResults(searchForResults(searchQuery));
 }
