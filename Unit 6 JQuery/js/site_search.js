dd709e76d6f8ad911310140bddf4c21d2c782e54
Dharak Verma <vermad1@mcmaster.ca>
2023-01-22 21:27:09 -0500

    Add page scraping search

--- Unit 6 JQuery/js/site_search.js
+++ Unit 6 JQuery/js/site_search.js
@@ -4,38 +4,39 @@ const data = [
         title: "Embedded Architecture",
         url: "embedded_projects/vehicle_architecture.html",
         description: "2020 - present competition vehicle embedded systems architecture, design decisions behind it, and details on each component.",
-        keywords: ["embedded", "architecture", "ECU", "electronic control unit", "STM", "ARM", "microcontroller", "MCU", "design", "decisions"]
     },
     {
         title: "Controls Architecture",
         url: "controls_projects/controls_architecture.html",
         description: "2022 - 2023 simulink-based vehicle control system architecture, design decisions behind it, and details on each component.",
-        keywords: ["controls", "architecture", "simulink", "matlab", "block design", "testing", "test", "white box", "black box", "decisions", "design"]
-    },
-    {
-        title: "Communications Manager",
-        url: "embedded_projects/work_in_progress.html",
-        description: "Communications layer on top of pre-existing hardware abstraction layer (HAL), used to modify and fix any shortcomings in the HAL and allow for extensive unit testing.",
-        keywords: ["C", "Python", "testing", "unit", "communications", "CAN", "I2C", "UART", "SPI", "STM"]
-    },
-    {
-        title: "Torque Path Controller",
-        url: "embedded_projects/work_in_progress.html",
-        description: "Torque controller component inside the Simulink based control system.",
-        keywords: ["torque", "controls", "architecture", "simulink", "matlab", "block design", "testing", "test", "white box", "black box", "decisions", "design", "controller", "motors"]
-    },
-    {
-        title: "Battery Monitor",
-        url: "embedded_projects/work_in_progress.html",
-        description: "Battery monitor component inside the Simulink based control system.",
-        keywords: ["controls", "architecture", "simulink", "matlab", "block design", "testing", "test", "white box", "black box", "decisions", "design", "BMS", "battery pack", "management"]
-    },
-    {
-        title: "Scheduler-From-Scratch",
-        url: "embedded_projects/work_in_progress.html",
-        description: "In-house designed and developed operating system scheduler, responsible for task management and pre-emption for all vehicle tasks.",
-        keywords: ["C", "STM", "designed", "developed", "operating system", "OS", "RTOS", "scheduler", "tasks"]
     }
+    // NOTE: Template for when the remaining projects are complete!
+    // {
+    //     title: "Communications Manager",
+    //     url: "embedded_projects/work_in_progress.html",
+    //     description: "Communications layer on top of pre-existing hardware abstraction layer (HAL), used to modify and fix any shortcomings in the HAL and allow for extensive unit testing.",
+    // },
+    // {
+    //     title: "Torque Path Controller",
+    //     url: "embedded_projects/work_in_progress.html",
+    //     description: "Torque controller component inside the Simulink based control system.",
+    // },
+    // {
+    //     title: "Battery Monitor",
+    //     url: "embedded_projects/work_in_progress.html",
+    //     description: "Battery monitor component inside the Simulink based control system.",
+    // },
+    // {
+    //     title: "Scheduler-From-Scratch",
+    //     url: "embedded_projects/work_in_progress.html",
+    //     description: "In-house designed and developed operating system scheduler, responsible for task management and pre-emption for all vehicle tasks.",
+    // }
+];
+
+// Array to store the project pages that should be searched for matching user input
+const pages = [
+    "embedded_projects/vehicle_architecture.html",
+    "controls_projects/controls_architecture.html"
 ];
 
 // searchForResults will search through the pages array and find matching results based on any relevant keyword or part of a keyword
@@ -69,31 +70,72 @@ function displayResults(results) {
 
         // Append results to search results container on webpage
         resultsContainer.appendChild(link);
-        description.innerText = "Try something related a Embedded Software or Controls project, such as 'ECU', 'Simulink', or 'C'.";
+        description.innerText = "Try something related to a Embedded Software or Controls project, such as 'ECU', 'Simulink', or 'C'.";
         resultsContainer.appendChild(description);
     } else {
         // Otherwise add each result to the results container
+        // result is a url to the relevant project page
         results.forEach(result => {
             // Have to repeat the element creation code as we want a new element for each search result
             link = document.createElement('a');
             description = document.createElement('p');
 
-            link.href = result.url;
-            link.innerText = result.title;
+            // Result already contains url
+            link.href = result;
 
+            // Must get the remaining properties for the search result container
+            // by searching data array and referencing returned object
+            projectInfo = data.find(obj => obj.url === result);
+
+            link.innerText = projectInfo.title;
             resultsContainer.appendChild(link);
-            description.innerText = result.description;
+            description.innerText = projectInfo.description;
             resultsContainer.appendChild(description);
         });
     }
 }
 
 // searchFormHandler will handle the search form submission on user input
-function searchFormHandler() {
-    // Capture the input field
-    input = document.getElementById("searchInput");
+// NOTE: must be async function in order to get results array properly
+async function searchFormHandler(event) {
+    // Prevent refresh on search form submission
+    event.preventDefault();
     // Store the search query
-    searchQuery = input.value;
-    
-    displayResults(searchForResults(searchQuery));
+    searchQuery = searchElement.search_input.value;
+
+    // Must wait for results array in searchPages to finish population, as it a async job
+    results = await searchPages(searchQuery);
+    displayResults(results);
 }
+
+async function searchPages(input) {
+    // Create a regular expression from the input searchQuery
+    // Make all input text lowercase for accuracy
+    regexValue = new RegExp(input, 'i');
+    // Find and return all webpages that match the search result
+    // Search through the entire HTML webpage
+    // Declare results as a const to keep accessible through program 
+    const results = [];
+
+    // Iterate through each url from data array and find any matching words from the relevant html page
+    for(d of data) {
+        // Make a GET request to each URL in the data array
+        // NOTE: GET is an asynchronous method, so we must wait on the computation before returning the results array
+        page = await $.get(d.url);
+
+        // Search the HTML page for matching words
+        if (page.match(regexValue)) {
+            results.push(d.url);
+        }
+    }
+
+    return results;
+}
+
+// Add an event listener for the submit button
+// This will ensure the contents of the entire page are loaded before we 
+// store the search_bar element
+document.addEventListener("DOMContentLoaded", function(){
+    searchElement = document.getElementById("search_bar");
+    searchElement.addEventListener('submit', searchFormHandler);
+});
