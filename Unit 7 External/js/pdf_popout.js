c4f2c0f8143db365fc8bf3114368e02aa894ae3f
Dharak Verma <vermad1@mcmaster.ca>
2023-04-12 21:16:19 -0400

    Fix file structure for github pages

--- Unit 7 External/js/pdf_popout.js
+++
@@ -1,6 +0,0 @@
-// NOTE: We are required to wait for the document to be ready to append before 
-// displaying or we have undefined behaviour
-$(document).ready(function() {
-    // Append iframe to pdf_popout div
-    $("#pdf_popout").append("<iframe src='../files/dharak_verma_resume.pdf'></iframe>");
-});
